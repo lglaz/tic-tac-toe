e2095ea8d49f5e0db3571f87ff7e62ca0fbc5e9d
Łukasz Głaz <lukasz.glaz@gmail.com>
2022-08-29 23:04:38 +0200

    Nuxt 3 simple api call

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+import {useFetch} from "nuxt/app";
+
+const data = await useFetch("/api/games");
+
+
+</script>
+
+
+
 <template>
   <div>
-    <NuxtWelcome />
+    {{data }}
   </div>
 </template>
